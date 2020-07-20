05abe1660edc317c026576aad5969d3fe8ca1041
masha <57405750+Massshhhaaa@users.noreply.github.com>
2020-07-20 16:09:07 +0300

    f
    f

---
+++ mainapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Hello, Team, Person
+
+admin.site.register(Hello)
+admin.site.register(Team)
+admin.site.register(Person)
---
+++ mainapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Hello, Team, Person
+
+
+# Create your views here.
+def main_def(request):
+    hello = Hello.objects.all()
+    return render(request, 'mainapp/index.html', context={'hello' :hello})
+
+def detail(request, slug):
+    post = get_object_or_404(Hello, slug = slug)
+    return render(request, "mainapp/post.html", {'post': post})
+
+
+def team_view(request):
+    teams= Team.objects.all()
+    persons = Person.objects.all()
+    return render(request, 'mainapp/teams.html', context={'teams' :teams, 'persons' :persons})
