f6d3dd7842fbd8cc49872a8a08f9ab782cd2bb26
masha <57405750+Massshhhaaa@users.noreply.github.com>
2020-07-21 16:26:30 +0300

    вава
    в

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -2,23 +2,12 @@ from django.contrib import admin
 from .models import Categories, Subgroup, SubgroupImage
 
 
-# 
-# class ImageAdmin(admin.StackedInline):
-#     model = Image
-#     extra = 0
-#
-# class CategoriesAdmin(admin.ModelAdmin):
-#     form = CategoriesForm
-#     inlines = [ImageInline]
-#
-#     def save_model(self, request, obj, form, change):
-#         super(CategoriesAdmin,self).save_model(request, obj, form, change)
-#         # obj.save()
-#
-#         for afile in request.FILES.getlist('photos_multiple'):
-#             obj.images.create(file=afile)
-#
-#
-# @admin.register(Categories)
-# class CategoriesAdmin(admin.ModelAdmin):
-#     inlines = [PostImage]
+class SubgroupImageInline(admin.TabulaInline):
+    model = SubgroupImage
+    extra = 0
+
+class SubgroupAdmin(admin.ModelAdmin):
+    inlines = [SubgroupImageInline,]
+
+admin.site.register(Categories)
+admin.site.register(Subgroup, SubgroupAdmin)
