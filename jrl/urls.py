ef6660318b01ee9c03f8944450fa1c774b45c5e1
masha <57405750+Massshhhaaa@users.noreply.github.com>
2020-07-24 08:07:51 +0300

    django jet connected

--- jrl/urls.py
+++ jrl/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('jet_api/', include('jet_django.urls')),
     path('', main_def, name='main_def'),
     path('admin/', admin.site.urls),
     path('<path:slug>/', SubgroupDetailView),
