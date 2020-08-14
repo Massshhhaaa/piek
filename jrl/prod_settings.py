d7d43ee29b356a0382e964e257572b812eea2798
vlat <kondensat2@gmail.com>
2020-08-14 20:40:24 +0300

    cvbcvb
    cvbc

--- jrl/prod_settings.py
+++ jrl/prod_settings.py
@@ -31,5 +31,5 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = '/static'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static'
+STATIC_ROOT = '/static2/'
