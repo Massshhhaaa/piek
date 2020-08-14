5601f7f4f6a998293969201ef91c86999ba5c424
vlat <kondensat2@gmail.com>
2020-08-14 21:23:44 +0300

    ааа

--- jrl/prod_settings.py
+++ jrl/prod_settings.py
@@ -2,8 +2,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-JET_PROJECT = 'piek'
-JET_TOKEN = '89f45bf8-c275-43f6-ab51-7ecbd30c9732'
+
 
 SECRET_KEY = 'kr3u3@2s8$a_&3oy$q-$=1woj5vso!%q1(mm7%2(1itq#ig-16'
 
