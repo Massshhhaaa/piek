d2fe51d56e8af277ac652f7fad3dfb8a2db2f805
vlat <kondensat2@gmail.com>
2020-08-14 20:22:43 +0300

    collect
    asd

--- jrl/prod_settings.py
+++ jrl/prod_settings.py
@@ -27,6 +27,7 @@ DATABASES = {
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     'piek/static/',
+    '/piek/venv/lib/python3.7/site-packages/django/contrib/admin/static/admin/',
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 MEDIA_URL = '/media/'
