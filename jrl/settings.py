84ffd71e2bfc7709fd9d04ffc293e95917fa3c43
masha <57405750+Massshhhaaa@users.noreply.github.com>
2020-08-22 03:09:24 +0300

    Revert "fd"
    This reverts commit e83ca3dad3212d5953c055973cb00df8cb82192d.

--- jrl/settings.py
+++ jrl/settings.py
@@ -39,8 +39,8 @@ TINYMCE_DEFAULT_CONFIG = {
     'min_height': '300',
 }
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'loseev5@gmail.com'
-EMAIL_HOST_PASSWORD = 'MzIir200503vgMRA-2041G'
+EMAIL_HOST_USER = 'kondensat01@gmail.com'
+EMAIL_HOST_PASSWORD = 'vladjous321'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAI_USE_SSL = False
