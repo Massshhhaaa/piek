e11027c1f14fe4d976b4a0c9485d659ef29e466c
masha <57405750+Massshhhaaa@users.noreply.github.com>
2020-08-27 01:56:56 +0300

    updated django admin interface

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,22 +1,29 @@
 from django.contrib import admin
 from .models import Group, Product, ProductImage, Modification, ProductDocs
+from django.utils.translation import ugettext_lazy as _
 
-
-
-class ProductImageInline(admin.TabularInline):
+class ProductImageInline(admin.StackedInline):
     model = ProductImage
-    extra = 1
+    extra = 0
+
+class ModificationInline(admin.StackedInline):
+    exclude = ('slug_mod',)
+    model = Modification
+    extra = 0
+
+class ProductDocsInline(admin.StackedInline):
+    model = ProductDocs
+    extra = 0
 
 class ProductAdmin(admin.ModelAdmin):
-    inlines = [ProductImageInline,]
-    list_display = ('href_title', 'parent')
+    list_filter = ('parent',)
+    inlines = [ProductImageInline, ModificationInline, ProductDocsInline]
+    list_display = ('href_title', 'parent', 'slug_product',)
 
 class ModificationAdmin(admin.ModelAdmin):
-    list_display = ('title', 'parent', 'slug_mod')
-    # exclude = ('slug_mod',)
-    pass
+    exclude = ('slug_mod',)
 
-admin.site.register(Group)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Group)
 admin.site.register(Modification, ModificationAdmin)
 admin.site.register(ProductDocs)
