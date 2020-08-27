c30f699dcb20c9bde859a037720daab55d410716
masha <57405750+Massshhhaaa@users.noreply.github.com>
2020-08-27 15:42:22 +0300

    d

--- mainapp/models.py
+++ mainapp/models.py
@@ -59,6 +59,7 @@ class Modification(models.Model):
     title    = models.CharField(max_length=250, )
     slug_mod = models.SlugField('url', null=True, blank=True, help_text='заполняется автоматически от title')
     content  = HTMLField(null=True, blank=True)
+    quantity = models.IntegerField(default=434)
 
     class Meta:
         ordering = ['parent__id']
