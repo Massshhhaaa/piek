fab25b7a04a3d642f8bd7725aaee92028aeb671d
masha <57405750+Massshhhaaa@users.noreply.github.com>
2020-09-05 12:04:59 +0300

    minor update

--- mainapp/views.py
+++ mainapp/views.py
@@ -49,11 +49,13 @@ def ModificationDetailView(request, slug, slug_product, slug_mod):
     product         = Product.objects.filter(parent__slug=slug, slug_product=slug_product).values('pk')
     product_content = Product.objects.only('content', 'href_title', 'h1_mod').get(pk__in = product)
     photos          = ProductImage.objects.filter(page__pk__in=product)
+    docs            = ProductDocs.objects.filter(page__pk__in=product)
     mod             = get_object_or_404(Modification, slug_mod=slug_mod, parent__pk__in=product)
 
     context = {
     'mod' : mod,
     'photos': photos,
+    'docs' : docs,
     'product_content' : product_content,
     'in_cart_counter': cart_counter(request),}
     return render(request, 'mainapp/ModificationDetailView.html', context)
