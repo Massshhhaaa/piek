a78f50113ed77d24927666723f3478e23eee677d
masha <57405750+Massshhhaaa@users.noreply.github.com>
2020-10-14 19:33:51 +0300

    minor

--- static/js/ajax/ajax.cart.js
+++ static/js/ajax/ajax.cart.js
@@ -2,8 +2,10 @@ const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
 console.log("csrf_token: "+ csrftoken);
 	function CartCounter(data){
 		$('#js-cart-counter').html(data.in_cart_counter);
+		$('#js-mobile-cart-counter').html(data.in_cart_counter);
     $('#js-in-cart-counter').html(data.in_cart_counter);
-			if ((document.getElementById('js-cart-counter').textContent || document.getElementById('js-cart-counter').innerText) ==0){document.getElementById('js-cart-counter-wrap').style.display = "none";}else{document.getElementById('js-cart-counter-wrap').style.display = "inline";}
+			if ((document.getElementById('js-cart-counter').textContent || document.getElementById('js-cart-counter').innerText) ==0){document.getElementById('js-cart-counter-wrap').style.display = "none";}else{document.getElementById('js-cart-counter-wrap').style.display = "inline";};
+			if ((document.getElementById('js-mobile-cart-counter').textContent || document.getElementById('js-mobile-cart-counter').innerText) ==0){document.getElementById('js-mobile-cart-counter-wrap').style.display = "none";}else{document.getElementById('js-mobile-cart-counter-wrap').style.display = "inline";}
 	}
 
   $(".js-conventional_designation").change(function ()  {
