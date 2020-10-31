51e651a0a21d1b5555f5369c972a1ab1f6839f73
masha <57405750+Massshhhaaa@users.noreply.github.com>
2020-10-31 13:39:02 +0300

    minor

--- mainapp/views_cart.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, get_object_or_404, HttpResponseRedirect, redirect, reverse
-from django.views.decorators.http import require_POST
-from django.conf import settings
-from mainapp.models import *
-from mainapp.views_cart import *
-import mainapp.views_cart
-from django.template.loader import render_to_string
-from django.core.mail import EmailMessage
-from django.http import JsonResponse
