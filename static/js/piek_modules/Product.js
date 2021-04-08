132377d94597c84508444f66c2a823747073239f
masha <57405750+Massshhhaaa@users.noreply.github.com>
2021-04-08 20:56:43 +0300

    rewrite prodmod css on sass and upgrade tinymce

--- static/js/piek_modules/Product.js
+++ static/js/piek_modules/Product.js
@@ -1,16 +1,1 @@
 //btn add to cart animation
-$(".js-cart-buttons").click(function () {
-var id  = "#"+ $(this).attr('id');
-console.log(id);
-$(id).addClass("btn-animate");
-});
-
-
-$(function(){
-	PhotosWidth();
-	ArrangementCartIcons();//calling function on window load
-	$(window).resize(function(){
-		PhotosWidth();
-		ArrangementCartIcons();//calling function on window resize
-	});
-});
